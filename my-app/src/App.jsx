fd4fc6606cffe013fdd043f670c7b9df1dbda6a1
Ina Meyer <ina.meyer@stud.h-da.de>
2022-03-01 21:00:13 +0100

    socket io error

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,36 +1,37 @@
-import "./App.css";
-import React, { Component } from "react";
+import './App.css'
+import React, { Component } from 'react'
+import { io } from 'socket.io-client'
 
-import Menu from "./Components/Menu";
-import Tests from "./Pages/Tests";
-import Question from "./Components/Cards/Question";
-import Anmeldung from "./Pages/Anmeldung";
-import Startseite from "./Pages/Startseite";
-import Austausch from "./Pages/Austausch";
+import Menu from './Components/Menu'
+import Tests from './Pages/Tests'
+import Question from './Components/Cards/Question'
+import Anmeldung from './Pages/Anmeldung'
+import Startseite from './Pages/Startseite'
+import Austausch from './Pages/Austausch'
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      firstAnnahme: "",
-      language: "",
-    };
-  }
+	constructor(props) {
+		super(props)
+		this.state = {
+			firstAnnahme: '',
+			language: '',
+		}
+	}
 
-  render() {
-    return (
-      <div className="App">
-        <Menu />
-        <Startseite />
-        <Anmeldung submitForm={(formData) => this.setState(formData)} />
-        <Tests />
-        <Austausch
-          language={this.state.language}
-          firstAnnahme={this.state.firstAnnahme}
-        />
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="App">
+				<Menu />
+				<Startseite />
+				<Anmeldung submitForm={(formData) => this.setState(formData)} />
+				<Tests />
+				<Austausch
+					language={this.state.language}
+					firstAnnahme={this.state.firstAnnahme}
+				/>
+			</div>
+		)
+	}
 }
 
-export default App;
+export default App
--- my-app/src/Pages/Austausch.jsx
+++ my-app/src/Pages/Austausch.jsx
@@ -1,20 +1,56 @@
-import React, { Component } from "react";
-import Match from "../Components/Match";
+import React, { Component } from 'react'
+import Match from '../Components/Match'
+import { io } from 'socket.io-client'
+import './Austausch.css'
+import Chat from '..Chat/index.'
 
 class Austausch extends Component {
-  state = {};
-  render() {
-    return (
-      <>
-        <Match language={this.props.language} />
-        <section id="part_5" tabIndex="5">
-          <h2>Test</h2>
-          {this.props.firstAnnahme}
-          {this.props.language}
-        </section>
-      </>
-    );
-  }
+	constructor(props) {
+		super(props)
+
+		let socket = io()
+
+		let messages = document.getElementById('messages')
+		let form = document.getElementById('form')
+		let input = document.getElementById('input')
+
+		form.addEventListener('submit', function (e) {
+			e.preventDefault()
+			if (input.value) {
+				socket.emit('chat message', input.value)
+				input.value = ''
+				console.log('huhu')
+			}
+		})
+
+		socket.on('chat message', function (msg) {
+			var item = document.createElement('li')
+			item.textContent = msg
+			messages.appendChild(item)
+			window.scrollTo(0, document.body.scrollHeight)
+		})
+
+		this.state = {}
+	}
+
+	render() {
+		return (
+			<>
+				<Match language={this.props.language} />
+				<section id="part_5" tabIndex="5">
+					<h2>Test</h2>
+					{this.props.firstAnnahme}
+					{this.props.language}
+
+					<ul id="messages"></ul>
+					<form id="form" action="">
+						<input id="input" autocomplete="off" />
+						<button>Send</button>
+					</form>
+				</section>
+			</>
+		)
+	}
 }
 
-export default Austausch;
+export default Austausch
